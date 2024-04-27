f401029b1f8833e94f26da632961e5b7a485ab1f
DistantDig <jameswaller711@gmail.com>
2024-04-27 14:12:32 -0700

    Added block sets and mod tab

--- src/main/java/net/birb_feather/imagine_castles/ImagineCastles.java
+++ src/main/java/net/birb_feather/imagine_castles/ImagineCastles.java
@@ -1,8 +1,12 @@
 package net.birb_feather.imagine_castles;
 
 import net.distantdig.ezLib.EzLib;
-import net.fabricmc.api.ModInitializer;
-
+import net.distantdig.ezLib.block.EzBlockSets;
+import net.distantdig.ezLib.block.EzBlocksBuilder;
+import net.distantdig.ezLib.item.EzItemGroups;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.PressurePlateBlock;
+import net.minecraft.world.level.block.state.properties.BlockSetType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,11 +20,50 @@ public class ImagineCastles extends EzLib {
 	}
 
 	@Override
-	public void registerModBlocks() {
+	public void registerModGroup() {
+		EzItemGroups.registerItemGroup(EzBlocksBuilder.inventoryMap.get("black_slate"));
 	}
 
 	@Override
-	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+	public void registerModBlocks() {
+		registerCastleStone("limestone");
+		registerCastleStone("white_slate");
+		registerCastleStone("black_slate");
+		registerCastleStone("orange_slate");
+		registerCastleStone("yellow_slate");
+		registerCastleStone("red_slate");
+		registerCastleStone("blue_slate");
+		registerCastleStone("light_blue_slate");
+		registerCastleStone("cyan_slate");
+		registerCastleStone("lime_slate");
+		registerCastleStone("grey_slate");
+		registerCastleStone("light_grey_slate");
+		registerCastleStone("purple_slate");
+		registerCastleStone("magenta_slate");
+		registerCastleStone("brown_slate");
+		registerCastleStone("green_slate");
+		registerCastleStone("pink_slate");
+		registerCastleStone("claystone");
+		registerCastleStone("antrecite");
+		registerCastleStone("shale");
+		registerCastleStone("chalk");
+		registerCastleStone("talk");
+	}
+
+	public void registerCastleStone(String name) {
+		new EzBlocksBuilder(name, Blocks.STONE, null, EzBlocksBuilder.EzMaterial.stone)
+				.pickaxe()
+				.stair()
+				.slab()
+				.verticalSlab()
+				.button(BlockSetType.STONE, 10, false)
+				.pressurePlate(BlockSetType.STONE, PressurePlateBlock.Sensitivity.EVERYTHING)
+				.pillar("gothic_", "_pillar", null)
+				.pillar("ornate_", "_pillar", null)
+				.extraBlock("chiseled_", null, null)
+				.extraBlock("cut_", null, null)
+				.wall();
+		new EzBlockSets().simpleStoneSet(name + "_bricks");
+		new EzBlockSets().simpleStoneSet("polished_" + name);
 	}
 }
