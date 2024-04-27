ee83ca5097d055dbfa011b38d54a91fffd9d2ec5
DistantDig <jameswaller711@gmail.com>
2024-04-27 13:56:38 -0700

    setup datagen to use EzLib

--- src/main/java/net/birb_feather/imagine_castles/ImagineCastlesDataGenerator.java
+++ src/main/java/net/birb_feather/imagine_castles/ImagineCastlesDataGenerator.java
@@ -1,11 +1,18 @@
 package net.birb_feather.imagine_castles;
 
+import net.distantdig.ezLib.EzLibDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.core.RegistrySetBuilder;
 
 public class ImagineCastlesDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		EzLibDataGenerator.runDataGen(fabricDataGenerator);
+	}
 
+	@Override
+	public void buildRegistry(RegistrySetBuilder registryBuilder) {
+		EzLibDataGenerator.runBuildRegistry(registryBuilder);
 	}
 }
